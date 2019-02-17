89a90ab2b8b2f9db7b974e0a718b711ab5856bec
Unknown <lai225joseph@gmail.com>
2019-02-17 09:26:10 +0800

    quartz.net

---
+++ SMTPPractise/JobSendEmail.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMTPPractise
+{
+    public class JobSendEmail : IJob
+    {
+
+        public void Execute(IJobExecutionContext context)
+        {
+            var result = context;
+            var emailAddress = context.JobDetail.JobDataMap.Get("emailAddress").ToString();
+            var password = context.JobDetail.JobDataMap.Get("password").ToString();
+            var content = context.JobDetail.JobDataMap.Get("content").ToString();
+            string mailBodyhtml = $"<p>{context}</p>";
+            var msg = new MailMessage(emailAddress, emailAddress, "Hello", mailBodyhtml);
+            msg.IsBodyHtml = true;
+            var smtpClient = new SmtpClient("smtp.live.com", 587); //if your from email address is "from@hotmail.com" then host should be "smtp.hotmail.com"**
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(emailAddress, password);
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(msg);
+        }
+    }
+}
---
+++ SMTPPractise/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SMTPPractise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int aDaySecond = 60 * 60 * 24;
+            int aWeek = 7;
+            //1.0創建調度工廠
+            ISchedulerFactory factory = new StdSchedulerFactory();
+            //2.0通過工廠獲取調度器實例
+            IScheduler scheduler = factory.GetScheduler();
+            //3.0通過JobBuilder構建Job
+            IJobDetail job = JobBuilder.Create<JobSendEmail>().UsingJobData("emailAddress", tboHotmail.Text).UsingJobData("password", tboPassword.Text).UsingJobData("content", tboContent.Text).Build();
+            //4.0通過TriggerBuilder構建Trigger
+            ISimpleTrigger trigger = (ISimpleTrigger)TriggerBuilder.Create()
+                .WithSimpleSchedule(a => a.WithIntervalInSeconds(aDaySecond).WithRepeatCount(aWeek))
+                .Build();
+            //5.0組裝各個組件<Job,Trigger>
+            scheduler.ScheduleJob(job, trigger);
+            //6.0啟動
+            scheduler.Start();
+            Thread.Sleep(10000);
+            //7.0銷毀內置的Job和Trigger
+            scheduler.Shutdown(true);
+        }
+    }
+    
+}
